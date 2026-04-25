30fc340e5a35a735a2f12136cbd084468a1c1133
Invictus Navarchus <githubgpg.lumpiness807@passmail.com>
2026-04-25 18:08:41 +0700

    feat: implement main script

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,26 @@
 // @ts-ignore isolatedModules
-console.log('hello world');
+const PREFIX = '[deepseek-auto-expert]';
+const TARGET_URL = 'https://chat.deepseek.com/';
+
+function main () {
+	if (window.location.href !== TARGET_URL) {
+		console.log(`${PREFIX} Not on new chat page`);
+		return;
+	}
+
+    const selector = '[data-model-type="expert"]';
+    const targetElement: HTMLElement | null = document.querySelector(selector);
+    if (!targetElement) {   
+        console.log(`${PREFIX} Element not found`);
+        return;
+    }
+    if (targetElement.getAttribute('aria-checked') === 'true') {
+        console.log(`${PREFIX} Element is already checked`);
+        return;
+    }
+
+    // For some reason, the button require two .click() to take effect, but our persistent setInterval should handle it. 
+    targetElement.click();
+}
+
+setInterval(main, 500);
